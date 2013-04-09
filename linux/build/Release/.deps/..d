fe59d9b3e08204eb2d8f3bb9a09475d58c8a5113
Table1 Pairing Station <table1.mailonline@gmail.com>
2013-04-09 13:48:09 +0100

    Add linux binaries

---
+++ linux/build/Release/.deps/..d
@@ -0,0 +1,1 @@
+cmd_. := LD_LIBRARY_PATH=/home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/Release/lib.host:/home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/Release/lib.target:$$LD_LIBRARY_PATH; export LD_LIBRARY_PATH; cd ../.; sh libzk-build.sh
---
+++ linux/build/Release/.deps/Release/obj.target/zookeeper/src/node-zk.o.d
@@ -0,0 +1,54 @@
+cmd_Release/obj.target/zookeeper/src/node-zk.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' -I/home/pairing/.node-gyp/0.8.14/src -I/home/pairing/.node-gyp/0.8.14/deps/uv/include -I/home/pairing/.node-gyp/0.8.14/deps/v8/include -I/home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/zk/include/zookeeper  -fPIC -Wall -pthread -m64 -Wall -Werror -O0 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/zookeeper/src/node-zk.o.d.raw  -c -o Release/obj.target/zookeeper/src/node-zk.o ../src/node-zk.cpp
+Release/obj.target/zookeeper/src/node-zk.o: ../src/node-zk.cpp \
+ /home/pairing/.node-gyp/0.8.14/src/node.h \
+ /home/pairing/.node-gyp/0.8.14/deps/uv/include/uv.h \
+ /home/pairing/.node-gyp/0.8.14/deps/uv/include/ares.h \
+ /home/pairing/.node-gyp/0.8.14/deps/uv/include/ares_version.h \
+ /home/pairing/.node-gyp/0.8.14/deps/uv/include/uv-private/uv-unix.h \
+ /home/pairing/.node-gyp/0.8.14/deps/uv/include/uv-private/ngx-queue.h \
+ /home/pairing/.node-gyp/0.8.14/deps/uv/include/uv-private/ev.h \
+ /home/pairing/.node-gyp/0.8.14/deps/uv/include/uv-private/eio.h \
+ /home/pairing/.node-gyp/0.8.14/deps/v8/include/v8.h \
+ /home/pairing/.node-gyp/0.8.14/deps/v8/include/v8stdint.h \
+ /home/pairing/.node-gyp/0.8.14/src/node_object_wrap.h \
+ /home/pairing/.node-gyp/0.8.14/src/node.h \
+ /home/pairing/.node-gyp/0.8.14/src/ev-emul.h \
+ /home/pairing/.node-gyp/0.8.14/src/eio-emul.h \
+ /home/pairing/.node-gyp/0.8.14/src/node_buffer.h \
+ /home/pairing/.node-gyp/0.8.14/src/node_object_wrap.h \
+ /home/pairing/.node-gyp/0.8.14/deps/v8/include/v8-debug.h \
+ /home/pairing/.node-gyp/0.8.14/deps/v8/include/v8.h \
+ /home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/zk/include/zookeeper/zookeeper.h \
+ /home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/zk/include/zookeeper/proto.h \
+ /home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/zk/include/zookeeper/zookeeper_version.h \
+ /home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/zk/include/zookeeper/recordio.h \
+ /home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/zk/include/zookeeper/zookeeper.jute.h \
+ ../src/zk_log.h ../src/buffer_compat.h \
+ /home/pairing/.node-gyp/0.8.14/src/node_version.h
+../src/node-zk.cpp:
+/home/pairing/.node-gyp/0.8.14/src/node.h:
+/home/pairing/.node-gyp/0.8.14/deps/uv/include/uv.h:
+/home/pairing/.node-gyp/0.8.14/deps/uv/include/ares.h:
+/home/pairing/.node-gyp/0.8.14/deps/uv/include/ares_version.h:
+/home/pairing/.node-gyp/0.8.14/deps/uv/include/uv-private/uv-unix.h:
+/home/pairing/.node-gyp/0.8.14/deps/uv/include/uv-private/ngx-queue.h:
+/home/pairing/.node-gyp/0.8.14/deps/uv/include/uv-private/ev.h:
+/home/pairing/.node-gyp/0.8.14/deps/uv/include/uv-private/eio.h:
+/home/pairing/.node-gyp/0.8.14/deps/v8/include/v8.h:
+/home/pairing/.node-gyp/0.8.14/deps/v8/include/v8stdint.h:
+/home/pairing/.node-gyp/0.8.14/src/node_object_wrap.h:
+/home/pairing/.node-gyp/0.8.14/src/node.h:
+/home/pairing/.node-gyp/0.8.14/src/ev-emul.h:
+/home/pairing/.node-gyp/0.8.14/src/eio-emul.h:
+/home/pairing/.node-gyp/0.8.14/src/node_buffer.h:
+/home/pairing/.node-gyp/0.8.14/src/node_object_wrap.h:
+/home/pairing/.node-gyp/0.8.14/deps/v8/include/v8-debug.h:
+/home/pairing/.node-gyp/0.8.14/deps/v8/include/v8.h:
+/home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/zk/include/zookeeper/zookeeper.h:
+/home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/zk/include/zookeeper/proto.h:
+/home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/zk/include/zookeeper/zookeeper_version.h:
+/home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/zk/include/zookeeper/recordio.h:
+/home/pairing/development/node-zookeeper-binary/node_modules/zookeeper/build/zk/include/zookeeper/zookeeper.jute.h:
+../src/zk_log.h:
+../src/buffer_compat.h:
+/home/pairing/.node-gyp/0.8.14/src/node_version.h:
