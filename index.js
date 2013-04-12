71a00c2a3a30152e90e59d0d7afd38d6734e8e78
Pairing station Table 1 <table1.mailonline@gmail.com>
2013-04-12 10:45:05 +0100

    Matt, Carlos: pointing to the correct platform library folder

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var os = require('os');
 
 module.exports = require('./'+os.platform()+'-'+os.arch()+"/lib/zookeeper");
 module.exports.ZooKeeper = module.exports;  // for backwards compatibility
-module.exports.Promise = require('./'+os.platform()+'/lib/zk_promise');
+module.exports.Promise = require('./'+os.platform()+'-'+os.arch()+'/lib/zk_promise');
 
